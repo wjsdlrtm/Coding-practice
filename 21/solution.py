889ea868bf64495bf561b8a46d2428c147baa669
wjsdlrtm <wjsdlrtm@gmail.com>
2016-10-31 12:12:48 +0900

    21

---
+++ 21/solution.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        l3 = []
+        l3 = set(l1 + l2)
+        return list(l3)
